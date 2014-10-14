240685902d242991959af087745059c409cceb7e
kefkius <kefkius@mail.com>
2014-10-14 14:55:09 +0000

    change genesis block hash and b58 prefixes

--- src/storage.py
+++ src/storage.py
@@ -8,7 +8,7 @@ from processor import print_log, logger
 from utils import bc_address_to_hash_160, hash_160_to_pubkey_address, hex_to_int, int_to_hex, Hash
 
 global GENESIS_HASH
-GENESIS_HASH = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+GENESIS_HASH = '00000c7c73d8ce604178dae13f0fc6ec0be3275614366d44b1b4b5c6e238c60c'
 
 """
 Patricia tree for hashing unspents
