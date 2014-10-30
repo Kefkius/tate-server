b8111da64a1c9dfacec4bcc8297c4682ce9e2c2a
Kefkius <kefkius@mail.com>
2014-10-30 12:29:08 -0400

    change files/pathnames and electrum->tate

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup
 
 setup(
-    name="electrum-server",
+    name="tate-server",
     version="0.9",
-    scripts=['run_electrum_server','electrum-server'],
+    scripts=['run_tate_server','tate-server'],
     install_requires=['plyvel','jsonrpclib', 'irc'],
     package_dir={
-        'electrumserver':'src'
+        'tateserver':'src'
         },
     py_modules=[
-        'electrumserver.__init__',
-        'electrumserver.utils',
-        'electrumserver.storage',
-        'electrumserver.deserialize',
-        'electrumserver.networks',
-        'electrumserver.blockchain_processor',
-        'electrumserver.server_processor',
-        'electrumserver.processor',
-        'electrumserver.version',
-        'electrumserver.ircthread',
-        'electrumserver.stratum_tcp',
-        'electrumserver.stratum_http'
+        'tateserver.__init__',
+        'tateserver.utils',
+        'tateserver.storage',
+        'tateserver.deserialize',
+        'tateserver.networks',
+        'tateserver.blockchain_processor',
+        'tateserver.server_processor',
+        'tateserver.processor',
+        'tateserver.version',
+        'tateserver.ircthread',
+        'tateserver.stratum_tcp',
+        'tateserver.stratum_http'
     ],
-    description="Bitcoin Electrum Server",
+    description="Mazacoin Tate Server",
     author="Thomas Voegtlin",
     author_email="thomasv1@gmx.de",
     license="GNU Affero GPLv3",
     url="https://github.com/spesmilo/electrum-server/",
-    long_description="""Server for the Electrum Lightweight Bitcoin Wallet"""
+    long_description="""Server for the Tate lightweight Mazacoin wallet. Forked by mazaclub from Electrum-server."""
 )
 
 
